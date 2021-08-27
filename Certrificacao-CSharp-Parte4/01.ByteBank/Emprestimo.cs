d6eb81467bfe7f3409be08cd5e1de658cb97a7fd
Rodrigo Bertozzi de Castro <rbertozzicastro1@hotmail.com>
2021-08-27 18:39:18 -0300

    Terceira alteração

--- Certrificacao-CSharp-Parte4/01.ByteBank/Emprestimo.cs
+++ Certrificacao-CSharp-Parte4/01.ByteBank/Emprestimo.cs
@@ -37,6 +37,10 @@ namespace _01.ByteBank
                 this.codigoContrato = codigoContrato;
                 Console.WriteLine($"Novo empréstimo com código: {codigoContrato}");
             }
+            else
+            {
+                //lançar uma exceção
+            }
             
         }
 
@@ -53,7 +57,11 @@ namespace _01.ByteBank
                 //se o novo prazo for maior que o prazo máximo,
                 //lançar um evento de "prazo estourado"
                 //senão, definir o novo prazo.
-
+                if (value > PRAZO_MAXIMO_PAGAMENTO_ANOS)
+                {
+                    OnPrazoMaximoEstourado?.Invoke(this, new EventArgs());
+                    return;
+                }
                 prazo = value;
                 Console.WriteLine($"novo prazo: {prazo}");
             }
@@ -69,6 +77,19 @@ namespace _01.ByteBank
             //   1.1) senão, se o prazo for maior que 5 
             //        E o valor for maior que 7 mil, a taxa é 7,5%
             //   1.2) senão, a taxa de juros é 8,75%
+
+            if (prazo > 0 && prazo < 5 && valor < 7000)
+            {
+                taxaJuros = 0.035m;
+            }
+            else if(prazo > 5 && valor > 7000)
+            {
+                taxaJuros = 0.075m;                
+            }
+            else
+            {
+                taxaJuros = 0.0875m;
+            }
 
             valorJuros = valor * taxaJuros * prazo;
             Console.WriteLine($"valorJuros: {valorJuros}");
--- Certrificacao-CSharp-Parte4/01.ByteBank/Program.cs
+++ Certrificacao-CSharp-Parte4/01.ByteBank/Program.cs
@@ -14,9 +14,23 @@ namespace _01.ByteBank
             //conta.SacarDinheiro(150);
 
             Emprestimo emprestimo = new Emprestimo("A1234");
+            emprestimo.OnPrazoMaximoEstourado += Emprestimo_OnPrazoMaximoEstourado;
+            emprestimo.Prazo = 3;
+            emprestimo.Prazo = 7;
+            emprestimo.CalcularJuros(6000, 3);
+            emprestimo.CalcularJuros(8500, 6);
+            emprestimo.CalcularJuros(11500, 4);
+
+            CaixaEletronico caixa = new CaixaEletronico();
+            caixa.Sacar(80);
+
+            Console.ReadKey();
         }
 
-
+        private static void Emprestimo_OnPrazoMaximoEstourado(object source, EventArgs e)
+        {
+            Console.WriteLine("Prazo estourado");
+        }
 
         private static void TestesContaBancaria()
         {
