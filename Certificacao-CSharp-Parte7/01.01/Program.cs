c8e779e76b123c39bc9886bf70af987682611fee
Rodrigo Bertozzi Castro <rbertozzicastro1@hotmail.com>
2021-09-02 11:21:38 -0300

    Alteração

---
+++ Certificacao-CSharp-Parte7/01.01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Campainha campainha = new Campainha();
+            campainha.Tocar();
+            Console.ReadKey();
+        }
+    }
+
+    class Campainha
+    {
+        public Action OnCampainhaTocou { get; set; }
+
+        public void Tocar()
+        {
+            if (OnCampainhaTocou != null)
+            {
+                OnCampainhaTocou();
+            }
+            
+        }
+    }
+}
